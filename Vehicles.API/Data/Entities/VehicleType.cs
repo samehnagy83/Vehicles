6e49f9e8c8d648148b06ac239ca7075eeea26f3f
samehnagy83 <samehnagy333@gmail.com>
2021-10-10 13:22:06 +0300

    Add Vehicle Type

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Vehicle Type")]
+        [MaxLength(50, ErrorMessage = "Field {0} cannot have more than {1} characters.")]
+        [Required(ErrorMessage = "Field {0} is required.")]
+        public string Description { get; set; }
+    }
+}
